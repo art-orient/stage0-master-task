015c239b9886a8ed6587f98427b0e46fdcd721cd
art-orient <69873430+art-orient@users.noreply.github.com>
2023-01-03 23:59:32 +0300

    Update Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    String getDescription() {
+        String article = hasFur ? "a" : "no";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and " +
+                article + " fur.";
+    }
 }
