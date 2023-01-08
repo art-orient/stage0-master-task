92284d8f0446dc022a9ee60353f7dfb4044e539d
Aliaksandr Artsikhovich <art-orient@tut.by>
2023-01-08 12:41:51 +0300

    loops done

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,16 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
+                if (cathetusLength - j <= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,21 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
+                if (cathetusLength - j <= i) {
+                    System.out.print(cathetusLength - j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 2; j <= cathetusLength; j++) {
+                if (j < i + 1) {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
